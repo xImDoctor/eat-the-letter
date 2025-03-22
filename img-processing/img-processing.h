76eb2a54916e662dd7ef85ac563ed98050bd4759
Andrew 'ImDoctor <andrew@imdoc.ru>
2025-03-22 10:40:55 +0300

    Add image to gray one convertation

--- img-processing/img-processing.h
+++ img-processing/img-processing.h
@@ -1,9 +1,33 @@
 #ifndef _IMAGE_PREPROCESSING_H_
 #define _IMAGE_PREPROCESSING_H_
 
+#include "../ascii_preprocessing.h"
+
 #include <FreeImage.h>
+#include <iostream>
 
 
+//#define PROCESS_IMAGE(file, output) \
+    (getExtension(file) == "png" ? processImagePNG(file, output) : processImageJPEG(file, output))
+
+#define PROCESS_IMAGE(file, output) \
+    do { \
+        if (imdoc::getImageExtension(file) == "png") { \
+            processImagePNG(file, output); \
+        } else if (imdoc::getImageExtension(file) == "jpg") { \
+            processImageJPEG(file, output); \
+        } \
+        else { \
+            std::cerr << "Ошибка: не подходящее расширение файла " << file << '\n'; \
+            std::exit(-1); \
+        } \
+    } while(0)
+
+
+
+void processImagePNG(const char* inputPath, const char* outputPath);
+
+void processImageJPEG(const char* inputPath, const char* outputPath);
 
 
 #endif
--- img-processing/img_processing.cpp
+++ img-processing/img_processing.cpp
@@ -1,3 +1,51 @@
 #include "img-processing.h"
 
 
+void processImagePNG(const char* inputPath, const char* outputPath) {
+    FreeImage_Initialise();
+
+    // load PNG
+    FIBITMAP* bitmap = FreeImage_Load(FIF_PNG, inputPath, PNG_DEFAULT);
+    if (!bitmap) {
+        std::cerr << "Ошибка загрузки изображения\n";
+        return;
+    }
+
+    FIBITMAP* grayBitmap = FreeImage_ConvertToGreyscale(bitmap);    // convert to gray map
+    FreeImage_Unload(bitmap);                                       // free original image
+
+    // save result
+    if (FreeImage_Save(FIF_PNG, grayBitmap, outputPath, PNG_DEFAULT))
+        std::cout << "Файл сохранен: " << outputPath << std::endl;
+    else
+        std::cerr << "Ошибка сохранения!\n";
+
+    // free memory
+    FreeImage_Unload(grayBitmap);
+    FreeImage_DeInitialise();
+}
+
+
+void processImageJPEG(const char* inputPath, const char* outputPath) {
+    FreeImage_Initialise();
+
+    // load png
+    FIBITMAP* bitmap = FreeImage_Load(FIF_JPEG, inputPath, JPEG_DEFAULT);
+    if (!bitmap) {
+        std::cerr << "Ошибка загрузки изображения\n";
+        return;
+    }
+
+    FIBITMAP* grayBitmap = FreeImage_ConvertToGreyscale(bitmap);    // convert to gray map
+    FreeImage_Unload(bitmap);                                       // free original image
+
+    // save gray result
+    if (FreeImage_Save(FIF_JPEG, grayBitmap, outputPath, JPEG_DEFAULT))
+        std::cout << "Файл сохранен: " << outputPath << std::endl;
+    else
+        std::cerr << "Ошибка сохранения!\n";
+
+    // free memory
+    FreeImage_Unload(grayBitmap);
+    FreeImage_DeInitialise();
+}
