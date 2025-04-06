1d2f3013e280f7f62e44d2153a7ac155faf4b14e
Andrew 'ImDoctor <andrew@imdoc.ru>
2025-04-06 22:51:24 +0300

    Add comments to defined macro

--- img-processing/img-processing.h
+++ img-processing/img-processing.h
@@ -10,6 +10,12 @@
 //#define PROCESS_IMAGE(file, output) \
     (getExtension(file) == "png" ? processImagePNG(file, output) : processImageJPEG(file, output))
 
+
+/*  PROCESS_IMAGE chooses correct function to process image to gray one using file extension. Better to use relative path.
+    file - const char* inputPath - input image file,
+    output - const char* outputPath - output image file.
+
+    Macros contains processImagePNG, processImageJPEG. */
 #define PROCESS_IMAGE(file, output) \
         if (imdoc::getImageExtension(file) == "png") { \
             imdoc::processImagePNG(file, output); \
@@ -21,7 +27,15 @@
             std::exit(-1); \
         } \
 
-
+/*
+        RESCALE_IMAGE chooses correct function to rescale image by scale factor times. Better to use relative path.
+        file - const char* imagePath - input image file,
+        output - const char* outputPath - output image file.
+        scale - int scaleFactor - rescaling factor (rescale by scaleFactor times)
+        scaleChange - flag (0 or 1) - if scaleChange is 0 than user scale is not counted using default scaleFactor=5 instead.
+        
+        Macros contains rescaleImagePNG, rescaleImageJPEG with various parameters depending on scaleChange flag.
+*/
 #define RESCALE_IMAGE(file, output, scale, changeScale) \
         if (imdoc::getImageExtension(file) == "png") { \
             if (!changeScale) \
@@ -40,6 +54,14 @@
         } 
 
 
+/*
+        TRANSFORM_IMAGE_TO_ASCII chooses correct function to transform the image processed to gray-colored one into ascii-art (using char symbols) and write
+        it to output file.
+        processedFile - const char* imagePath - input image file processed to gray colors,
+        outputTextFile - std::ofstream& outputFile - output text file to write ascii-art in.
+
+        Macros contains transformPNGtoASCII, transformJPGtoASCII.
+*/
 #define TRANSFORM_IMAGE_TO_ASCII(processedFile, outputTextFile) \
         if (imdoc::getImageExtension(processedFile) == "png") { \
             imdoc::transformPNGtoASCII(processedFile, outputTextFile); \
